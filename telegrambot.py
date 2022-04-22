9956205d84cb86322202d44f51e9f82867245f56
Bruno Mayerle Leite <brleite@gmail.com>
2022-04-22 13:27:55 -0300

    Inclusão de funcionalidade para envio de imagem e documento

--- telegrambot.py
+++ telegrambot.py
@@ -15,3 +15,34 @@ def send_message(chat_id, token, message, parse_mode='html'):
     response = requests.post('https://api.telegram.org/bot' + token + '/sendMessage', data, verify=False)
 
     return response.status_code
+
+def send_image(chat_id, token, image_url):
+    """Send message to Telegram BOT
+    Args:
+        chat_id (int): chat_id number: -111111
+        token [str]: Token to HTTP API: 11111111:AAAAAAAAA
+        image_url (str): Image url
+    Returns:
+        str : Response Status Code
+    """
+    data = {'chat_id':chat_id, 'photo':image_url}
+    response = requests.post('https://api.telegram.org/bot' + token + '/sendPhoto', data, verify=False)
+
+    return response.status_code
+
+
+def send_document(chat_id, token, document_url, file_description, parse_mode='html'):
+    """Send message to Telegram BOT
+    Args:
+        chat_id (int): chat_id number: -111111
+        token [str]: Token to HTTP API: 11111111:AAAAAAAAA
+        document_url (str): Document url
+        file_description (str): File Description
+        parse_mode (str): (Default=html) Use html or markdown
+    Returns:
+        str : Response Status Code
+    """
+    data = {'chat_id':chat_id, 'document':document_url, 'caption':file_description, 'parse_mode':parse_mode}
+    response = requests.post('https://api.telegram.org/bot' + token + '/sendDocument', data, verify=False)
+
+    return response.status_code
